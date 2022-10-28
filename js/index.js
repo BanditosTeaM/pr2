82e30da13fb6cb2a038289447ff0772c83a33a02
BanditosTeaM <74429724+BanditosTeaM@users.noreply.github.com>
2022-10-28 20:00:07 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ new Swiper('.swiper-container', {
     sliderPerView: 1,
     loop: true,
     autoplay: true,
+    effect: 'cards',
     grabCursor: true,
     navigation: {
         nextEl: '.swiper-button-next',
